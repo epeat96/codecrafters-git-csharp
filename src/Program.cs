bed22f6c146c57b0d4077b2981d160e9a5d0215a
Cristobal Barreto <44922559+epeat96@users.noreply.github.com>
2026-09-12 21:16:29 -0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -47,8 +47,10 @@ switch (command)
         }
 
         var parentDir = sb.Append(hash.First(), hash.Skip(1).First()).ToString();
+        Console.WriteLine($"ParentDir: {parentDir}");
         sb = sb.Clear();
         var remaining = sb.Insert(0, hash.Skip(2)).ToString();
+        Console.WriteLine($"Remaining: {remaining}");
 
         Debug.Assert(remaining != null, nameof(remaining) + " != null");
         Debug.Assert(parentDir != null, nameof(parentDir) + " != null");
