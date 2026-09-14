6243f8f539d7239723674097fcdcc64bb44f9296
Cristobal Barreto <44922559+epeat96@users.noreply.github.com>
2026-09-14 15:15:20 -0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -75,7 +75,7 @@ switch (command)
 
         Debug.Assert(remaining != null, nameof(remaining) + " != null");
         Debug.Assert(parentDir != null, nameof(parentDir) + " != null");
-        ZlibHelper.CompressFile(hash, objectsDirectory, FileHelper.GetFileContent(filePath));
+        ZlibHelper.CompressFile(hash, objectsDirectory, BlobFileHelper.GetBlobContentFromFile(filePath));
         break;
     }
     default:
